69265e0c0eda387f6ecbffd75d29b1fe1088d8e9
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2020-12-22 16:15:34 -0600

    like stylesheet

--- ninja_app/views.py
+++ ninja_app/views.py
@@ -7,7 +7,20 @@ def home(request):
     return render(request, 'index.html')
 
 def process_money(request):
+    if 'counter' not in request.session:
+        request.session['counter'] = 0
     if request.method == 'GET':
         return redirect('')
     elif request.method == 'POST':
-        print("Post request")
+        if request.POST['location'] == 'farm':
+            gold = random.randint(10, 20)*1
+            request.session['counter'] += gold
+        elif request.POST['location'] == 'cave':
+            gold = random.randint(5, 10)*1
+            request.session['counter'] += gold
+        elif request.POST['location'] == 'house':
+            gold = random.randint(2, 5)*1
+            request.session['counter'] += gold
+        elif request.POST['location'] == 'casino':
+            gold = random.randint(0, 50)*1
+            request.session['counter'] += gold
