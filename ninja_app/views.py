aca7fe81428890b71b826cdf1eaf0ba6cc3d6f3c
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2020-12-21 18:33:28 -0600

    add process money initial code

--- ninja_app/views.py
+++ ninja_app/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def process_money(request):
+    if request.method == 'GET':
+        return redirect('')
+    if request.method == 'POST':
